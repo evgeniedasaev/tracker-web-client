d4c1926b9935bfacad2fcf30a5219da157eb8d97
Evgeniy Dasaev <jackensteiny@gmail.com>
2026-01-09 14:19:17 +0400

    chore: set up dev tools

---
+++ .husky/_/husky.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+if [ -z "$husky_skip_init" ]; then
+  debug() {
+    [ "$HUSKY_DEBUG" = "1" ] && echo "husky (debug) - $1"
+  }
+
+  readonly hook_name="$(basename "$0")"
+  debug "starting $hook_name..."
+
+  if [ "$HUSKY" = "0" ]; then
+    debug "HUSKY env variable is set to 0, skipping hook"
+    exit 0
+  fi
+
+  if [ -r ~/.huskyrc ]; then
+    debug "sourcing ~/.huskyrc"
+    . ~/.huskyrc
+  fi
+
+  export readonly husky_skip_init=1
+  sh -e "$0" "$@"
+  exitCode="$?"
+  debug "finished $hook_name, exit code $exitCode"
+  exit "$exitCode"
+fi
